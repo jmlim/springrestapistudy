0f4b6f6ba423be9163d21a642aa793bc0d40bf1f
Lim Jeong muk <hackerljm@gmail.com>
2019-03-10 23:19:06 +0900

    이벤트 조회 API 구현 및 테스트 작성.

--- src/test/java/io/jmlim/springrestapistudy/events/EventControllerTest.java
+++ src/test/java/io/jmlim/springrestapistudy/events/EventControllerTest.java
@@ -320,13 +320,39 @@ public class EventControllerTest {
         ;
     }
 
-    private void generateEvent(int i) {
+    @Test
+    @TestDescription("기존의 이벤트를 하나 조회하기")
+    public void getEvent() throws Exception {
+        //Given
+        Event event = this.generateEvent(100);
+
+        //When & then
+        this.mockMvc.perform(get("/api/events/{id}", event.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").exists())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.profile").exists())
+                .andDo(document("get-an-event"))
+                // 문서화 필요하나 생략...
+        ;
+    }
+
+    @Test
+    @TestDescription("없는 이벤트를 조회했을 때 404 응답받기")
+    public void getEvent404() throws Exception {
+        //When & then
+        this.mockMvc.perform(get("/api/events/12345"))
+                .andExpect(status().isNotFound());
+    }
+
+    private Event generateEvent(int index) {
         Event event = Event.builder()
-                .name("event " + i)
+                .name("event " + index)
                 .description("test event")
                 .build();
 
-        this.eventRepository.save(event);
+        return this.eventRepository.save(event);
     }
 }
 
