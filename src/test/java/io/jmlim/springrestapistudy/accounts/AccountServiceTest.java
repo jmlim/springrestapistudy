28a87da51303dfc5a9f4521cc37fed1118a66f8b
Lim Jeong muk <hackerljm@gmail.com>
2019-03-12 10:00:03 +0900

    스프링 시큐리티 예외 테스트 부분 추가.

--- src/test/java/io/jmlim/springrestapistudy/accounts/AccountServiceTest.java
+++ src/test/java/io/jmlim/springrestapistudy/accounts/AccountServiceTest.java
@@ -1,11 +1,15 @@
 package io.jmlim.springrestapistudy.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,11 +18,15 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -44,5 +52,41 @@ public class AccountServiceTest {
 
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    /*
+     *  이렇게만 하면 예외의 타입밖에 확인 못함.
+     *  */
+    /*@Test(expected = UsernameNotFoundException.class)
+    public void findByUsernameFail() {
+        String username = "jmlim@eyeq.co.kr";
+        accountService.loadUserByUsername(username);
+    }*/
+
+    /**
+     * 더 많은것을 테스트 할 수 있으나 코드가 장황해짐.
+     */
+   /* @Test
+    public void findByUsernameFail() {
+        String username = "jmlim@eyeq.co.kr";
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        } catch (UsernameNotFoundException e) {
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }*/
+    /**
+     * 더 많은것을 테스트 할 수 있으나 코드가 장황해짐.
+     */
+    @Test
+    public void findByUsernameFail() {
+        // Expected
+        String username = "jmlim@eyeq.co.kr";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        //When
+        accountService.loadUserByUsername(username);
     }
 }
